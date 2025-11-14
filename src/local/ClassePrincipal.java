c788f44e9aaa7303e7726796e4a7815c6079e2b7
vikir342 <vikiretec@gmail.com>
2025-11-14 10:44:21 -0300

    adicionado men final

--- src/local/ClassePrincipal.java
+++ src/local/ClassePrincipal.java
@@ -1,5 +1,7 @@
 package local;
 
+import java.util.Scanner;
+
 public class ClassePrincipal {
 	
 	public static void limparTela() {
@@ -16,8 +18,31 @@ public class ClassePrincipal {
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Scanner sc = new Scanner(System.in);
+		
+		int opcao;
+		
+		do{
+			limparTela();
+			System.out.println("menu de opção");
+			System.out.println("[1] adiconar um aluno");
+			System.out.println("[2] Excluir um aluno");
+			System.out.println("[3] ver alunos");
+			System.out.println("[4] Sair");
+			
+			opcao = sc.nextInt();
+			sc.nextLine();
+			
+			limparTela();
+		
+			switch(opcao){
+			
+			case 1:
+				System.out.println("selecionado opção 1");
+				System.out.println("selecionado opção 1");
+			}
+		} while(opcao != 4);
 	}
 
 }
